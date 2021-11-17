fbd4ae783ec229b881c0612f3498764250cd98ba
Rob <zeijden@gmail.com>
2021-11-17 12:39:31 +0100

    added temps per month, little redesign

--- src/components/WiWaWet.vue
+++ src/components/WiWaWet.vue
@@ -1,23 +1,31 @@
 <template>
   <div>
-    <v-container v-if="ventilate">
-      <v-icon x-large color="ma-5" class="mr-1">mdi-weather-windy</v-icon>Ventilate!
-    </v-container>
-    <v-container v-if="!ventilate">
-      <v-icon x-large color="ma-5" class="mr-1">mdi-window-close</v-icon>Close Window!
-    </v-container>
     <v-container class="grey lighten-5">
       <v-row no-gutters>
         <v-col
             cols="12"
-            sm="12"
+            sm="2"
         >
-          <v-card
-              class="pa-2"
-              tile
-          >
-            <v-icon large color="ma-5" class="mr-1">mdi-home</v-icon>Inside
-          </v-card>
+            <v-icon large color="info" class="mr-1">mdi-home</v-icon>Inside
+          
+        </v-col>
+        <v-col
+            cols="12"
+            sm="8"
+        >          
+        </v-col>
+        <v-col
+            cols="12"
+            sm="2"
+            align="right"
+        >
+                <div v-if="ventilate">
+      <v-icon x-large color="info" class="mr-1">mdi-weather-windy</v-icon>
+    </div>
+    <div v-if="!ventilate">
+      <v-icon x-large color="error" class="mr-1">mdi-window-close</v-icon>
+    </div>
+          
         </v-col>
         <v-col
             cols="12"
@@ -31,8 +39,8 @@
                 v-model="tempInside.val"
                 :thumb-color="tempInside.color"
                 thumb-label="always"
-                min="10"
-                max="30"
+                :min="minTempInside"
+                :max="maxTempInside"
             >
               <template v-slot:prepend>
                 <v-icon large
@@ -96,14 +104,9 @@
       <v-row no-gutters>
         <v-col
             cols="12"
-            sm="12"
+            sm="2"
         >
-          <v-card
-              class="pa-2"
-              tile
-          >
             <v-icon large color="success ma-5" class="mr-1">mdi-pine-tree</v-icon>Outside
-          </v-card>
         </v-col>
         <v-col
             cols="12"
@@ -117,8 +120,8 @@
                 v-model="tempOutside.val"
                 :thumb-color="tempOutside.color"
                 thumb-label="always"
-                min="10"
-                max="30"
+                :min="minTempOutSide[month]"
+                :max="maxTempOutSide[month]"
             >
               <template v-slot:prepend>
                 <v-icon large
@@ -186,8 +189,16 @@ export default {
   props: {
     msg: String
   },
+  mounted() {
+    this.tempOutside.val = (this.maxTempOutSide[this.month] + this.minTempOutSide[this.month]) / 2
+  },
   data() {
     return {
+      month: new Date().getMonth()+1,
+      minTempOutSide: {1: 0, 2: 0, 3: 0, 4: 5, 5: 5, 6: 10, 7: 10, 8: 10, 9: 10, 10: 5, 11: 0, 12: 0},
+      maxTempOutSide: {1: 15, 2: 15, 3: 15, 4: 20, 5: 25, 6: 25, 7: 30, 8: 30, 9: 30, 10: 25, 11: 20, 12: 15},
+      minTempInside: 10,
+      maxTempInside: 30,
       tempInside: {label: 'color', val: 20, color: 'red'},
       tempOutside: {label: 'track-color', val: 20, color: 'red'},
       humInside: {label: 'thumb-color', val: 50, color: 'info'},
@@ -196,7 +207,15 @@ export default {
   },
   computed: {
     ventilate: function () {
-      return this.humOutside.val - 3 * (this.tempInside.val - this.tempOutside.val) < this.humInside.val
+      var ti = this.tempInside.val
+      var ri = this.humInside.val
+      var ahi = 13.2471*Math.pow(Math.E,17.67*ti/(ti+243.5))*ri/(273.15+ti);
+
+      var to = this.tempOutside.val
+      var ro = this.humOutside.val
+      var aho = 13.2471*Math.pow(Math.E,17.67*to/(to+243.5))*ro/(273.15+to);
+      return ahi > aho
+      
     },
     absHumInside: function () {
       var t = this.tempInside.val
